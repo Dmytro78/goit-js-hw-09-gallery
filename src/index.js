0f44242254509ecbe5e107ed14334549727d7d75
Dmitriy Liashchenko <fttravel78@gmail.com>
2021-06-20 21:10:36 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import "./sass/main.scss";
 import "./js/gallery";
-import "./js/carousel-gallery";
--- src/js/carousel-gallery.js
+++ src/js/carousel-gallery.js
@@ -1,63 +1,25 @@
-// const img = document.querySelectorAll(".gallery__image");
-// const arrayImages = [];
+import lightBoxImg  from './gallery';
+import images from './gallery-items.js';
 
-// img.forEach((el) => {
-//   arrayImages.push(el.getAttribute("data-source"));
-// });
+
+const imagesLength = images.length - 1;
+const lightboxImageRef = document.querySelector('.lightbox__image');
 
 function scrolling(evt) {
-  let newIndex;
-  const currentId = arrayImages.indexOf(lightBoxImg.src);
-  if (evt.key === "ArrowLeft") {
-    newIndex = currentId - 1;
-    if (newIndex == -1) {
-      newIndex = arrayImages.length - 1;
+  let index = images.findIndex(item => item.original === lightboxImageRef.getAttribute('src'));
+
+  if (evt.key === 'ArrowLeft') {
+    index -= 1;
+    if (index < 0) {
+      index = imagesLength;
     }
-  } else if (evt.key === "ArrowRight") {
-    newIndex = currentId + 1;
-    if (newIndex === arrayImages.length) {
-      newIndex = 0;
+  } else if (evt.key === 'ArrowRight') {
+    index += 1;
+    if (index > imagesLength) {
+      index = 0;
     }
   }
-  lightBoxImg.src = arrayImages[newIndex];
+  lightBoxImg.src = images[index].original;
 }
 
 export default scrolling;
-
-
-/*
-const carousel = {
-  scrolling: function (evt) {
-  let newIndex;
-  const currentId = arrayImages.indexOf(lightBoxImg.src);
-  if (evt.key === "ArrowLeft") {
-    newIndex = currentId - 1;
-    if (newIndex == -1) {
-      newIndex = arrayImages.length - 1;
-    }
-  } else if (evt.key === "ArrowRight") {
-    newIndex = currentId + 1;
-    if (newIndex === arrayImages.length) {
-      newIndex = 0;
-    }
-  }
-  lightBoxImg.src = arrayImages[newIndex];
-},
-
-closeModalOverlay: function (e) {
-  if (e.carrentTarget === e.target) {
-    isCloseModal(e);
-  }  
-},
-
-closeEsc: function (e) {
-  if (e.code === "Escape") {
-    isCloseModal(e);
-  }  
-},
-}
-
-export const scrolling = carousel.scrolling;
-export const closeModalOverlay = carousel.closeModalOverlay;
-export const closeEsc = carousel.closeEsc;
-*/
